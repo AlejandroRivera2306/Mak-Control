95889130541768db18288ee116799d94e25eb0e5
AlejandroRivera2306 <ale_jandro2306@hotmail.com>
2023-10-19 14:24:07 -0500

    autenticacion de usuarios

---
+++ backend/models/Empresa.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+
+
+const empresaSchema = mongoose.Schema({
+
+    nombre: {
+        type: String,
+        trim: true,
+        required: true,
+
+    },
+
+    descripcion: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+
+    fechaEntrega: {
+
+        type: Date,
+        default: Date.now(),
+
+    },
+
+    cliente: {
+        type: String,
+        trim: true,
+        required: true,
+
+    },
+
+    creador: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Usuario"
+    },
+
+    colaboradores : [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"Usuario",
+
+        }
+
+    ]
+
+
+
+
+
+
+},{
+
+    timestamps: true, 
+})
+
+const Empresa = mongoose.model("Empresa", empresaSchema );
+export default Empresa;
